cb14367072d3833b87c48fadffe066d53de0b1b7
Hank Hogan <ysh2291939848@outlook.com>
2023-11-22 19:21:46 +0800

    Initial Commit

---
+++ DesignCode15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignCode15
+//
+//  Created by Hank Hogan on 2023/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignCode15/DesignCode15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode15App.swift
+//  DesignCode15
+//
+//  Created by Hank Hogan on 2023/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
